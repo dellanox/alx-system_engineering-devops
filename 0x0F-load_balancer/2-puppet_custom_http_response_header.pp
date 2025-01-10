425ae99628790a55eeeff9ccf177c79c4e261119
dellanox <ozioma@gmail.com>
2025-01-10 12:59:09 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # configuration with puppet
 
-exec {	'http header':
-	command => 'sudo apt-get update -y;
-	sudo apt-get install nginx -y;
-	sudo sed -i "/server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-enabled/default
-	sudo service nginx restart'
-	provider => shell, 
+exec { 'configure_nginx':
+  command => 'sudo apt-get update -y &&
+              sudo apt-get install nginx -y &&
+              sudo sed -i "/server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-enabled/default &&
+              sudo service nginx restart',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  unless  => 'grep -q "add_header X-Served-By" /etc/nginx/sites-enabled/default',
 }
